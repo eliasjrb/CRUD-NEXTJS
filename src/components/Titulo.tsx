ae538044cdd3113b784ac8f7c3e638cfafcbbb2c
94563624268 <94563624268@EPR-020000045>
2021-09-23 12:08:23 -0400

    principio da pagina

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,9 @@
+
+export default function Titulo(props) {
+    return(
+        <div>
+            <h1>{props.children}</h1>
+            <hr />
+        </div>
+    )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+
+export default function Home() {
+  return (
+    <div className={`
+      flex h-screen justify-center 
+      items-center bg-gradient-to-r from-blue-500 to-purple-600
+      text-white
+    `}>
+       <Layout titulo="Cadastro Simples">
+         <span>Conteudo..</span>
+       </Layout>
+    </div>
+  )
+}
